13aa89e478f940934ec411b212cd367a8b97846e
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-11 00:02:46 +0530

    firebase config added

---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBu-sGsiAukDNyzUQPQuAO0cE6nYcjtyn0",
+    authDomain: "clone-bed4e.firebaseapp.com",
+    databaseURL: "https://clone-bed4e.firebaseio.com",
+    projectId: "clone-bed4e",
+    storageBucket: "clone-bed4e.appspot.com",
+    messagingSenderId: "527939308747",
+    appId: "1:527939308747:web:21a6b0250367aeb3149f4e",
+    measurementId: "G-BV8GZ4CX3Y"
+  };
